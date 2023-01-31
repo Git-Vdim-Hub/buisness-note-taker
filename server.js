2b097446abda0b325834503633ddca7edbd98c28
Git-Vdim-Hub <vadimrace@gmail.com>
2023-01-30 19:11:57 -0500

    fixed error with public files in the wrong spots, got test text to render on the page through server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 //PSEUDO CODE
 //Server JS
-
+//FIGRURE OUT HOW TO WORK WITH TWO HTML FILES/CHECK LESSONS
 //2. we need a post and a get to api/notes (delete bonus)
 //-when talking post or get we are talking from client view
 //so we are getting data from the server and we are posting data
 //to the server
+//2a. get- need to get the page
 //we are working with a db that is is in the file tree with .json ext
 //so use .json
 //3. we also need the bodies for those two/three
@@ -23,14 +24,15 @@ app.use(express.urlencoded({ extened: true }));
 
 app.use(express.static('public'));
 
-app.get('/notes', (req,res) =>
-    res.sendFile(path.join(__dirname, './notes.html'))
-);
 
 
-app.get('*', (req,res) =>
-    res.sendFile(path.join(__dirname, './index.html'))
-);
+app.get('/api/notes', (req,res) => {
+    console.log("PikaPika")
+    res.json(db);
+})
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 app.listen(PORT, () =>
 console.log(`App listening at http://localhost:${PORT} 🚀`)
